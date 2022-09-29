615526359adc3cdf5a5f0c261d9b09761a110e26
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-09-29 20:07:32 +0700

    add codelab MyCustomView

---
+++ MyCustomView/app/src/main/java/com/intermediateandroid/mycustomview/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.intermediateandroid.mycustomview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.intermediateandroid.mycustomview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setMyButtonEnable()
+
+        binding.myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Do nothing.
+            }
+        })
+
+        binding.myButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                binding.myEditText.text,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = binding.myEditText.text
+        binding.myButton.isEnabled = result != null && result.toString().isNotEmpty()
+    }
+}
