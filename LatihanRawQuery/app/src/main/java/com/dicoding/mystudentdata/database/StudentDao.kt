c43b7e6c4120d2780e553da8654c8deb0edb8883
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-11-01 15:46:41 +0700

    add Room Raw Query Codelab

---
+++ LatihanRawQuery/app/src/main/java/com/dicoding/mystudentdata/database/StudentDao.kt
@@ -0,0 +1,35 @@
+package com.dicoding.mystudentdata.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import androidx.sqlite.db.SimpleSQLiteQuery
+
+@Dao
+interface StudentDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertStudent(student: List<Student>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertUniversity(university: List<University>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourse(course: List<Course>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourseStudentCrossRef(courseStudentCrossRef: List<CourseStudentCrossRef>)
+
+    @RawQuery(observedEntities = [Student::class])
+    fun getAllStudent(query: SimpleSQLiteQuery): LiveData<List<Student>>
+
+    @Transaction
+    @Query("SELECT * FROM student")
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>>
+
+    @Transaction
+    @Query("SELECT * FROM university")
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>>
+
+    @Transaction
+    @Query("SELECT * FROM student")
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>>
+}
